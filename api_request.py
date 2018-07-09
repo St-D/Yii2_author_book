fd53c0986611f76ed9796f536cbc9cde23434b5c
Stanislav <stastastas@mail.ru>
2018-07-10 01:54:38 +0300

    add exceptions

--- api_request.py
+++ api_request.py
@@ -1,4 +1,5 @@
-#  -*- coding: cp1251 -*-                                                                                             #
+# -*- coding: utf-8 -*-
+#                                                                                             #
 # Python 3.x.x
 
 import requests
@@ -19,100 +20,102 @@ def time_now():
     return time.strftime("%Y-%m-%d", time_tup)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# --------------------------------------------------------\n')
-print('-----------к заданию 4a----------------------------\n')
-
-api_url = SERVER_NAME + '/api/v1/books/list'
-res = requests.get(api_url)
-
-print('Status code: ', res.status_code)
-print('Headers: ', res.headers['Content-Type'])
-print('Request URL: ', res.url, '\n')
-
 try:
-    for k, v in res.json().items():
-        print(k, " : ",  v)
-except json.decoder.JSONDecodeError:
-    print(' Nothing to show :(( ')
-except AttributeError:
-    for k in res.json():
-        print(k)
+    # --------------------------------------------------------\n')
+    print('-----------Рє Р·Р°РґР°РЅРёСЋ 4a----------------------------\n')
 
-# --------------------------------------------------------\n')
-print('---------------------------------------------------\n')
+    api_url = SERVER_NAME + '/api/v1/books/list'
+    res = requests.get(api_url)
+
+    print('Status code: ', res.status_code)
+    print('Headers: ', res.headers['Content-Type'])
+    print('Request URL: ', res.url, '\n')
+
+    try:
+        for k, v in res.json().items():
+            print(k, " : ",  v)
+    except json.decoder.JSONDecodeError:
+        print(' Nothing to show :(( ')
+    except AttributeError:
+        for k in res.json():
+            print(k)
+
+    # --------------------------------------------------------\n')
+    print('---------------------------------------------------\n')
+
+    # --------------------------------------------------------\n')
+    print('-----------Рє Р·Р°РґР°РЅРёСЋ 4b----------------------------\n')
+    id = 2
+
+    api_url = SERVER_NAME + '/api/v1/books/2' + str(id)
+    res = requests.get(api_url)
+
+    print('Status code: ', res.status_code)
+    print('Headers: ', res.headers['Content-Type'])
+    print('Request URL: ', res.url, '\n')
+
+    try:
+        for k, v in res.json().items():
+            print(k, " : ",  v)
+        print(json.dumps(res.json(), sort_keys=True, indent=4))
+    except json.decoder.JSONDecodeError:
+        print(' Nothing to show :(( ')
+    except AttributeError:
+        for k in res.json():
+            print(k)
+    # --------------------------------------------------------\n')
+    print('---------------------------------------------------\n')
+
+    # --------------------------------------------------------\n')
+    print('-----------Рє Р·Р°РґР°РЅРёСЋ 4c----------------------------\n')
+    id = 30
+
+    api_url = SERVER_NAME + '/api/v1/books/update/' + str(id)
+    param_dict = {
+        "author_id": 2,
+        "title": "change_title_using_POST_requests",
+        "year_of_writing": time_now(),
+        }
+    res = requests.post(api_url, data=param_dict)
+
+    print('Status code: ', str(res.status_code) + '*')
+
+    if res.status_code == 200:
+        print('\t*Update successfully')
+    elif res.status_code == 404:
+        print('\t*Entry not found')
+    else:
+        print('\t*Failed to delete the object for unknown reason')
+
+    print('Headers: ', res.headers['Content-Type'])
+    print('Request URL: ', res.url, '\n')
+    # --------------------------------------------------------\n')
+    print('---------------------------------------------------\n')
+
+    # --------------------------------------------------------\n')
+    print('-----------Рє Р·Р°РґР°РЅРёСЋ 4d----------------------------\n')
+    id = 31
+
+    api_url = SERVER_NAME + '/api/v1/books/' + str(31)
+    res = requests.delete(api_url)
+
+    print('Status code: ', str(res.status_code) + '*')
+
+    if res.status_code == 204:
+        print('\t*Delete successfully')
+    elif res.status_code == 404:
+        print('\t*Entry not found')
+    else:
+        print('\t*Failed to delete the object for unknown reason')
+
+    print('Headers: ', res.headers['Content-Type'])
+    print('Request URL: ', res.url, '\n')
+    # --------------------------------------------------------\n')
+    print('---------------------------------------------------\n')
+
+except requests.exceptions.ConnectionError:
+    print(' ~ Run development YII server: $yii serve')
+    input('press <Enter>')
+    exit()
 
 
-# --------------------------------------------------------\n')
-print('-----------к заданию 4b----------------------------\n')
-id = 2
-
-api_url = SERVER_NAME + '/api/v1/books/2' + str(id)
-res = requests.get(api_url)
-
-print('Status code: ', res.status_code)
-print('Headers: ', res.headers['Content-Type'])
-print('Request URL: ', res.url, '\n')
-
-try:
-    for k, v in res.json().items():
-        print(k, " : ",  v)
-    print(json.dumps(res.json(), sort_keys=True, indent=4))
-except json.decoder.JSONDecodeError:
-    print(' Nothing to show :(( ')
-except AttributeError:
-    for k in res.json():
-        print(k)
-# --------------------------------------------------------\n')
-print('---------------------------------------------------\n')
-
-
-# --------------------------------------------------------\n')
-print('-----------к заданию 4c----------------------------\n')
-id = 30
-
-api_url = SERVER_NAME + '/api/v1/books/update/' + str(id)
-param_dict = {
-    "author_id": 2,
-    "title": "change_title_using_POST_requests",
-    "year_of_writing": time_now(),
-    }
-res = requests.post(api_url, data=param_dict)
-
-print('Status code: ', str(res.status_code) + '*')
-
-if res.status_code == 200:
-    print('\t*Update successfully')
-elif res.status_code == 404:
-    print('\t*Entry not found')
-else:
-    print('\t*Failed to delete the object for unknown reason')
-
-print('Headers: ', res.headers['Content-Type'])
-print('Request URL: ', res.url, '\n')
-# --------------------------------------------------------\n')
-print('---------------------------------------------------\n')
-
-
-# --------------------------------------------------------\n')
-print('-----------к заданию 4d----------------------------\n')
-id = 31
-
-api_url = SERVER_NAME + '/api/v1/books/' + str(31)
-res = requests.delete(api_url)
-
-print('Status code: ', str(res.status_code) + '*')
-
-if res.status_code == 204:
-    print('\t*Delete successfully')
-elif res.status_code == 404:
-    print('\t*Entry not found')
-else:
-    print('\t*Failed to delete the object for unknown reason')
-
-print('Headers: ', res.headers['Content-Type'])
-print('Request URL: ', res.url, '\n')
-# --------------------------------------------------------\n')
-print('---------------------------------------------------\n')
-
-input('press <Enter>')
-exit()

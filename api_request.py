20a45661b44b039901e93f330868c1ae0f132ffb
Stanislav <stastastas@mail.ru>
2018-07-10 01:38:53 +0300

    resolve conflict with *.py file

--- api_request.py
+++ api_request.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-#  -*- coding: cp1251 -*-  #
+#  -*- coding: cp1251 -*-                                                                                             #
 # Python 3.x.x
 
 import requests
@@ -117,37 +116,3 @@ print('---------------------------------------------------\n')
 
 input('press <Enter>')
 exit()
-=======
-#  -*- coding: cp1251 -*-  #
-# Python 3.x.x
-
-import requests
-import json
-
-api_url = 'http://localhost:8080/api/v1/books/list'
-
-param_dict = {
-    "id": 2,
-            }
-
-res = requests.get(api_url, params=param_dict)
-
-print(res.status_code)
-print(res.headers['Content-Type'])
-print(res.url)
-
-# print(res.json())
-
-try:
-    for k, v in res.json().items():
-        print(k, " : ",  v)
-
-    print(res.json())
-except json.decoder.JSONDecodeError:
-    print('Nothin to show :(')
-except AttributeError:
-    for k in res.json():
-        print(k)
-
-
->>>>>>> f2d04ba924ae04c6d2d1ce7565dfc84823e67533
